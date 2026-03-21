a90f2c6377ffbbab5f13e8fb733f7207134de5b4
Paulo Diogo <pndiogo@gmail.com>
2026-03-21 23:49:27 +0000

    chore: update dependencies to latest versions

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true
+  },
   plugins: [
-    tsconfigPaths(),
     tailwindcss(),
     tanstackRouter({
       target: "react",
